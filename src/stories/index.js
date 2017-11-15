4241c8a196b98cdbf665229d6837b02216be4f1e
Tom Coleman <tom@thesnail.org>
2017-11-15 12:07:27 +1100

    Ensure the boxes are always the same size.

--- src/stories/index.js
+++ src/stories/index.js
@@ -13,9 +13,12 @@ storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
+
+const RandomSpan = () => <span style={{display: 'inline-block', overflow: 'hidden', width: '100px'}} className="chromatic-ignore">{Math.random()}</span>
+
 storiesOf('Random', module)
   .add('number', () => <div className="chromatic-ignore">{Math.random()}</div>)
-  .add('three', () => <div><span className="chromatic-ignore">{Math.random()}</span> - <span className="chromatic-ignore">{Math.random()}</span> - <span className="chromatic-ignore">{Math.random()}</span></div>);
+  .add('three', () => <div><RandomSpan /> - <RandomSpan /> - <RandomSpan /></div>);
 
 storiesOf('gif', module)
   .add('image', () => <img className="chromatic-ignore" src="https://cdn.dribbble.com/users/46315/screenshots/1065283/icons-animation.gif"/>)
